885299ab5208f212dd6de7fa7ea13c139ad89082
kapawlak <kapawlak@ucsb.edu>
2022-01-14 15:27:03 -0800

    Revival commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module goMarkov
+
+go 1.17
+
+require github.com/pkg/profile v1.6.0 // indirect
--- main.go
+++ main.go
@@ -13,22 +13,26 @@ func main() {
 	defer profile.Start(profile.ProfilePath(".")).Stop()
 
 	steps, length, width := int(math.Pow(10, 5)), 1000, 1000
+
 	//Initialize a system
 	sys := NewSys(length, width, 1)
 
-	//Set Atoms
+	//Set Inital Atom Distributions
 	gridb := RandomPop(0, 5, length, width)
 	gridf := Circular(0.5,0.25,0.3,10,length,width)
 
-	b := DefineAtom(gridb, "bacteria", length, width)
-	f := DefineAtom(gridf, "food", length, width)
+	b := DefineAtom(gridb, "bacteria", length, width) //Add a "bacteria" entity
+	f := DefineAtom(gridf, "food", length, width)     //Add a "food" entity
 
+    //Add Diffusion Process
 	sys.SetHopProcess(b, 1.0)
-	//sys.SetHopProcess(f, 1.0)
+	sys.SetHopProcess(f, 1.0)
 
-	sys.SetConsumerProcess([]*Atom{b,f},0.1)
+    //Add interaction Processes
+	sys.SetConsumerProcess([]*Atom{b,f},0.1) //Let "bacteria" eat the "food" at rate 0.1
 	fmt.Println("At Start")
-	//b.Print()
+
+    //Start the simulation
 	start := time.Now()
 	Evolve(sys, steps, 10)
 	elapsed := time.Since(start)
